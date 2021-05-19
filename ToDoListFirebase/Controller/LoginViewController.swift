9402ecb5606ae880f8d05b9f2f1a916a074026b8
Nikita <immortalvelial@icloud.com>
2021-05-19 21:26:57 +0200

    initial

---
+++ ToDoListFirebase/Controller/LoginViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ToDoListFirebase
+//
+//  Created by Nikita on 19.05.21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var warnLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextFiel: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func loginTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func registerTapped(_ sender: UIButton) {
+    }
+}
+
---
+++ ToDoListFirebase/Controller/TasksViewController.swift
@@ -0,0 +1,45 @@
+//
+//  TasksViewController.swift
+//  ToDoListFirebase
+//
+//  Created by Nikita on 19.05.21.
+//
+
+import UIKit
+
+class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel?.text = "Test \(indexPath.row)"
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+
+    @IBAction func addTapped(_ sender: UIBarButtonItem) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
